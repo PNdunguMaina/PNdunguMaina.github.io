e84138a271c914bbb1283a504ae90cd04ac00ab0
PNdunguMaina <ndunguwamaina@gmail.com>
2022-06-22 18:00:17 +0300

    add background to match figma design

--- main.js
+++ main.js
@@ -2,13 +2,11 @@ const hamburger = document.querySelector(".menu-bar");
 const sideMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
   sideMenu.classList.toggle("active");
 });
 
-document.querySelectorAll(".nav-menu").forEach((n) =>
+document.querySelectorAll(".nav-link").forEach((n) =>
   n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    sideMenu.remove("active");
+    sideMenu.classList.remove("active");
   })
 );
