4e4b6c14800deb0eeeb7a153fc73996f23684eb8
PNdunguMaina <ndunguwamaina@gmail.com>
2022-06-25 17:57:49 +0300

    Use local storage to save user input

--- main.js
+++ main.js
@@ -10,3 +10,35 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     sideMenu.classList.remove("active");
   })
 );
+
+// create website data preservation 
+
+// object containing form data
+const formInputData = {
+  nameInputData: '',
+  emailInputData: '',
+  messageInputData: ''
+}
+
+// check the availability of form data
+if (localStorage.getItem('formInputData')!==null){
+  // retrieve the data if it is there
+  const inputData = localStorage.getItem('formInputData');
+  // deserialize the data
+  formInputData = JSON.parse('inpuData');
+}
+
+// obtain the input and message data
+const nameEmailMessageData = document.querySelectorAll('input, textarea');
+// loop through the fields
+nameEmailMessageData.forEach((field) => {
+  field.value = formInputData[field.name];
+  field.addEventListener('input', myLocalStorage);
+})
+
+// execute this function once event is fired
+
+function myLocalStorage(e) {
+  formInputData[e.target.name] = e.target.value;
+    localStorage.setItem('formInputData', JSON.stringify('formInputData'));
+}
